890995396c77a56ffe01d0734beca545a39eec8c
tom reilly <treilly94@live.com>
2020-03-04 18:29:47 +0000

    added vnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "spinnaker" {
   name     = "spinnaker"
-  location = "UK South"
+  location = "westus"
 }
 
 resource "azurerm_storage_account" "spinnaker" {
@@ -37,3 +37,26 @@ resource "local_file" "kubeconfig" {
   content  = azurerm_kubernetes_cluster.spinnaker.kube_config_raw
   filename = "../kubeconfig.yml"
 }
+
+resource "azurerm_virtual_network" "spinnaker" {
+  name                = "spinnaker"
+  location            = azurerm_resource_group.spinnaker.location
+  resource_group_name = azurerm_resource_group.spinnaker.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  subnet {
+    name           = "subnet3"
+    address_prefix = "10.0.3.0/24"
+  }
+}
+
