2557113371708dd3aa563c55804f8ce887b5c67d
tom reilly <treilly94@live.com>
2020-03-16 15:04:12 +0000

    changed azure.sh to read default ssh key

--- scripts/azure.sh
+++ scripts/azure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "What public ssh key should be used?"
-read SSHPUBKEY
+# If you dont already have a service principal run
+# az ad sp create-for-rbac --name "spinnaker"
 
 echo "What subscription id should be used?"
 read SUBSCRIPTION_ID
@@ -13,13 +13,17 @@ echo "What is the tennant id of your Service Principal?"
 read TENANT_ID
 
 RESOURCE_GROUP="spinnaker"
-VAULT_NAME=tomreillyspinnakervault
+VAULT_NAME="tomreillyspinnakervault"
 ACCOUNT="spinnakerazure"
+SSHPUBKEY=$( cat ~/.ssh/id_rsa.pub )
 
 az keyvault create --enabled-for-template-deployment true --resource-group $RESOURCE_GROUP --name $VAULT_NAME
 az keyvault set-policy --secret-permissions get --name $VAULT_NAME --spn $APP_ID
 az keyvault secret set --name VMUsername --vault-name $VAULT_NAME --value spinnaker
-az keyvault secret set --name VMSshPublicKey --vault-name $VAULT_NAME --value $SSHPUBKEY
+az keyvault secret set --name VMSshPublicKey --vault-name $VAULT_NAME --value "$SSHPUBKEY"
+
+# Wait for the above services to finish setup
+sleep 5s
 
 hal config provider azure enable
 
